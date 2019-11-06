34401db20dfdac58b625ab5b750347e2e3183d5d
Juan X. Gomez <juan.gomez@meredith.com>
2019-11-06 13:17:02 -0500

    adding description meta tag to index.html

--- src/components/DynamicContent/Projects/Projects.jsx
+++ src/components/DynamicContent/Projects/Projects.jsx
@@ -6,11 +6,20 @@ const Projects = () => {
         <div className="Section Projects">
             <h1>Projects</h1>
             <div className="indent">
+                <h1>xendke-io</h1>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo atque, consectetur, aspernatur temporibus quia nisi quisquam distinctio optio mollitia sequi provident debitis quod nam doloribus exercitationem alias, sit sunt inventore!</p>
+            </div>
+            <div className="indent">
+                <h1>digitaizer</h1>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo atque, consectetur, aspernatur temporibus quia nisi quisquam distinctio optio mollitia sequi provident debitis quod nam doloribus exercitationem alias, sit sunt inventore!</p>
+            </div>
+            <div className="indent">
+                <h1>pet-or-not</h1>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo atque, consectetur, aspernatur temporibus quia nisi quisquam distinctio optio mollitia sequi provident debitis quod nam doloribus exercitationem alias, sit sunt inventore!</p>
+            </div>
+            <div className="indent">
+                <h1>More</h1>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo atque, consectetur, aspernatur temporibus quia nisi quisquam distinctio optio mollitia sequi provident debitis quod nam doloribus exercitationem alias, sit sunt inventore!</p>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo atque, consectetur, aspernatur temporibus quia nisi quisquam distinctio optio mollitia sequi provident debitis quod nam doloribus exercitationem alias, sit sunt inventore!</p>   
             </div>
         </div>
     );
