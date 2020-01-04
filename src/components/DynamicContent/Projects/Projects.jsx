6f7c74eb9597f79add6785a4e34b8b6abc945ab7
Xavier Gomez <xendke@gmail.com>
2020-01-03 20:13:34 -0500

    using flex wrap for projects section and add some projects

--- src/components/DynamicContent/Projects/Projects.jsx
+++ src/components/DynamicContent/Projects/Projects.jsx
@@ -5,21 +5,31 @@ const Projects = () => {
     return (
         <div className="Section Projects">
             <h1>Projects</h1>
-            <div className="indent">
-                <h1>xendke-io</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo atque, consectetur, aspernatur temporibus quia nisi quisquam distinctio optio mollitia sequi provident debitis quod nam doloribus exercitationem alias, sit sunt inventore!</p>
-            </div>
-            <div className="indent">
-                <h1>digitaizer</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo atque, consectetur, aspernatur temporibus quia nisi quisquam distinctio optio mollitia sequi provident debitis quod nam doloribus exercitationem alias, sit sunt inventore!</p>
-            </div>
-            <div className="indent">
-                <h1>pet-or-not</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo atque, consectetur, aspernatur temporibus quia nisi quisquam distinctio optio mollitia sequi provident debitis quod nam doloribus exercitationem alias, sit sunt inventore!</p>
-            </div>
-            <div className="indent">
-                <h1>More</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo atque, consectetur, aspernatur temporibus quia nisi quisquam distinctio optio mollitia sequi provident debitis quod nam doloribus exercitationem alias, sit sunt inventore!</p>
+            <div className="flex">
+                <div className="indent">
+                    <h1>Synapse</h1>
+                    <p>I develop and maintain many web applications at Synapse. Here are some: <a href="https://magazinediscountcenter.com/" target="_blank" rel="noopener noreferrer">One</a> and <a href="https://staging-mdc.magazinediscountcenter.com/bizrateapp/#/split/BIZ600/webpromotion=20002001" target="_blank" rel="noopener noreferrer">Two</a>.</p>
+                </div>
+                <div className="indent">
+                    <h1>Xendke</h1>
+                    <p>The website you are on right now. This app is built with React and SCSS.</p>
+                </div>
+                <div className="indent">
+                    <h1>Catchup</h1>
+                    <p>An anonymous chat web app! React, SCSS, Firebase.</p>
+                </div>
+                <div className="indent">
+                    <h1>Digitaizer</h1>
+                    <p>Basic GUI and Neural Network to identify your handwritten digits! Python, Tkinter, Pandas.</p>
+                </div>
+                <div className="indent">
+                    <h1>Pet or Not</h1>
+                    <p>A simple web app that can recognize whether a pet exist in a given picture!</p>
+                </div>
+                <div className="indent">
+                    <h1>More</h1>
+                    <p>I have way more projects, check them all out <a href="https://github.com/xendke" target="_blank" rel="noopener noreferrer">here</a>.</p>
+                </div>
             </div>
         </div>
     );
