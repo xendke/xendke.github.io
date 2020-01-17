e92a428da9cd2c09a060584161dfb1c4f2465f33
Xavier Gomez <xendke@gmail.com>
2020-01-16 19:54:21 -0500

    use webp with png as backup for headshot

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import imageOfJuan from 'assets/self.png';
+import imageOfJuanWebP from 'assets/self.webp';
 import './TopBar.scss';
 
 const TopBar = () => {
     return (
         <div className="TopBar">
             <div>
-                <img
-                    className="avatar"
-                    src={imageOfJuan}
-                    alt="Picture of Juan smiling to the camera."
-                />
+                <picture>
+                    <source srcset={imageOfJuanWebP} type="image/webp" />
+                    <source srcset={imageOfJuan} type="image/png" /> 
+                    <img 
+                        className="avatar"
+                        src={imageOfJuan}
+                        alt="Picture of Juan smiling to the camera."
+                    />
+                </picture>
             </div>
             <div>
                 <p className="line name">Juan X. Gomez</p>
