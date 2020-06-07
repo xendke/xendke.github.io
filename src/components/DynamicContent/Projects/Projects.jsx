c29bfc34224b4791292d09b8f683a09e395d7ab8
Juan X Gomez <xendke@gmail.com>
2020-06-07 08:36:35 -0400

    update projects with links

--- src/components/DynamicContent/Projects/Projects.jsx
+++ src/components/DynamicContent/Projects/Projects.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import './Projects.scss';
 
+const Link = ({ href, children }) => (
+    <a href={href} target="_blank" rel="noopener noreferrer">
+        {children}
+    </a>
+)
+
 const Projects = () => {
     return (
         <div className="Section Projects">
@@ -8,7 +14,7 @@ const Projects = () => {
             <div className="flex">
                 <div className="indent">
                     <h1>Synapse</h1>
-                    <p>I develop and maintain many web applications at Synapse. Here are some: <a href="https://magazinediscountcenter.com/" target="_blank" rel="noopener noreferrer">One</a> and <a href="https://staging-mdc.magazinediscountcenter.com/bizrateapp/#/split/BIZ600/webpromotion=20002001" target="_blank" rel="noopener noreferrer">Two</a>.</p>
+                    <p>I developed and maintained many web applications at Synapse. Here are some examples: <Link href="https://magazinediscountcenter.com/">One</Link> and <Link href="https://mdc.magazinediscountcenter.com/bizrateapptest/#/split/BIZ3306/webpromotion=20003054">Two</Link>.</p>
                 </div>
                 <div className="indent">
                     <h1>Xendke</h1>
@@ -16,15 +22,15 @@ const Projects = () => {
                 </div>
                 <div className="indent">
                     <h1>Catchup</h1>
-                    <p>An anonymous chat web app! React, SCSS, Firebase.</p>
+                    <p>An anonymous chat web app! React, SCSS, Firebase. <Link href="https://github.com/xendke/catchup">Code</Link> & <Link href="https://catchup-chat.web.app">Demo</Link>.</p>
                 </div>
                 <div className="indent">
                     <h1>Digitaizer</h1>
-                    <p>Basic GUI and Neural Network to identify your handwritten digits! Python, Tkinter, Pandas.</p>
+                    <p>Basic GUI and Neural Network to identify your handwritten digits! Python, Tkinter, Pandas. <Link href="https://github.com/xendke/digitaizer">Code</Link>.</p>
                 </div>
                 <div className="indent">
                     <h1>Pet or Not</h1>
-                    <p>A simple web app that can recognize whether a pet exist in a given picture!</p>
+                    <p>A simple web app that can recognize whether a pet exist in a given image! <Link href="https://github.com/xendke/pet-or-not">Code</Link> & <Link href="https://pet-or-not.herokuapp.com/">Demo</Link></p>
                 </div>
                 <div className="indent">
                     <h1>More</h1>
