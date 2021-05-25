dba89435b2ebc97190650c7948e13b18a2146bb8
Juan X Gomez <xendke@gmail.com>
2021-05-24 20:32:32 -0400

    update animation

--- src/animation/index.js
+++ src/animation/index.js
@@ -1,12 +1,6 @@
 import * as THREE from 'three'
 import { WEBGL } from './helpers'
 
-// colors:
-// light: e8e8e8
-// dark: 222831
-// salmon: f05454
-// blue: 30475e
-
 const planeFactory = (color, index) => {
 	const geometry = new THREE.PlaneGeometry(100, 100, 30)
 	const material = new THREE.MeshBasicMaterial({
@@ -23,47 +17,60 @@ const planeFactory = (color, index) => {
 }
 
 const runAnimation = () => {
-	const background = new THREE.Color(0x222831)
-	const scene = new THREE.Scene()
 	const aspectRatio = window.innerWidth / window.innerHeight
 	const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000)
+	camera.position.setZ(5)
+	camera.position.setX(1)
 
 	const renderer = new THREE.WebGLRenderer({
 		canvas: document.getElementById('canvas'),
 	})
-
 	renderer.setPixelRatio(window.devicePixelRatio)
 	renderer.setSize(window.innerWidth, window.innerHeight)
-	camera.position.setZ(5)
+
+	const background = new THREE.Color(0x222831)
+	const scene = new THREE.Scene()
 	scene.background = background
+	scene.fog = new THREE.Fog(background, 1, 15)
 
 	const planes = [planeFactory(0xf05454, 0), planeFactory(0x30475e, 1)]
 	planes.forEach((plane) => {
 		scene.add(plane)
 	})
-	scene.fog = new THREE.Fog(background, 1, 15)
+	let planesVelocity = false
 
-	const movePlanes = () => {
-		const top = document.body.getBoundingClientRect().top
+	const movePlanes = (multiplier) => {
 		planes.forEach((plane, index) => {
 			const offset = index * 10
-			plane.rotation.z = top * 0.001 - offset
+			plane.rotation.z = multiplier * 0.001 - offset
 		})
 	}
-	document.body.onscroll = movePlanes
+	window.addEventListener(
+		'scroll',
+		() => movePlanes(document.body.getBoundingClientRect().top),
+		false
+	)
+	window.addEventListener(
+		'mousemove',
+		(event) => (planesVelocity = (event.pageX + event.pageY) / 1000000),
+		false
+	)
 
-	function onWindowResize() {
+	function resizeCanvas() {
 		camera.aspect = window.innerWidth / window.innerHeight
 		camera.updateProjectionMatrix()
 		renderer.setSize(window.innerWidth, window.innerHeight)
 	}
-	window.addEventListener('resize', onWindowResize, false)
+	window.addEventListener('resize', resizeCanvas, false)
 
 	const animate = () => {
 		requestAnimationFrame(animate)
 		planes.forEach((plane) => {
-			plane.rotation.x -= 0.0025
-			plane.rotation.y += 0.0025
+			plane.rotation.x -= 0.0015
+			plane.rotation.y += 0.0015
+			if (planesVelocity) {
+				plane.rotation.z += planesVelocity
+			}
 		})
 		renderer.render(scene, camera)
 	}
